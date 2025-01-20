58605c603d58d01c2b17c13e121982f6686dc16c
Romil Patel <romilpatel1223@gmail.com>
2025-01-20 10:19:25 +0530

    Changed the layout of the software

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
-from PySide6.QtWidgets import QApplication, QLabel, QWidget
-from PySide6.QtWidgets import QGridLayout, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QLabel, QWidget, QPushButton
+from PySide6.QtWidgets import QGridLayout, QVBoxLayout, QHBoxLayout
 from PySide6.QtGui import QFont
 from gui import GUI
 import sys
@@ -9,7 +9,18 @@ class construct_main(GUI):
     def __init__(self):
         super().__init__()
         self.central = QWidget()
+        self.text = QWidget()
+        self.btn_grp = QWidget()
+
+        self.test_btn = QPushButton("Take a Quick Test!")
+        self.lessn_btn = QPushButton("Lessons")
+        self.stats_btn = QPushButton("My Stats")
+        self.supp_btn = QPushButton("Support Us")
+
+        self.main_layout = QHBoxLayout()
         self.layout = QVBoxLayout()
+        self.btn_layout = QGridLayout()
+
         self.h_font = QFont(self.font, 40)
         self.p_font = QFont(self.font, 18)
 
@@ -20,9 +31,20 @@ class construct_main(GUI):
         self.desc.setFont(self.p_font)
 
         
+        self.btn_layout.addWidget(self.test_btn, 0, 0)
+        self.btn_layout.addWidget(self.lessn_btn, 0, 1)
+        self.btn_layout.addWidget(self.stats_btn, 1, 0)
+        self.btn_layout.addWidget(self.supp_btn, 1, 1)
+        self.btn_grp.setLayout(self.btn_layout)
+
         self.layout.addWidget(self.heading)
         self.layout.addWidget(self.desc)
-        self.central.setLayout(self.layout)
+        self.text.setLayout(self.layout)
+        
+        self.main_layout.addWidget(self.text)
+        self.main_layout.addWidget(self.btn_grp)
+
+        self.central.setLayout(self.main_layout)
         self.setCentralWidget(self.central)
 
 app = QApplication(sys.argv)
